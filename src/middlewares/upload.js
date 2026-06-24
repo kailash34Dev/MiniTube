fb1775d7c1ff7624806484ae9e0d1b0d225d5c9e
kailash phukan <kailashphukan34@gmail.com>
2026-06-24 17:58:00 +0530

    aws s3 added

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,20 +1,27 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { S3Client } from '@aws-sdk/client-s3';
 import { nanoid } from 'nanoid';
 import path from 'path';
-import fs from 'fs';
+import dotenv from 'dotenv';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadPath = 'uploads/videos/';
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath, { recursive: true });
-    }
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
+dotenv.config();
+
+export const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  }
+});
+
+const storage = multerS3({
+  s3: s3,
+  bucket: process.env.AWS_S3_BUCKET_NAME,
+  key: (req, file, cb) => {
     const id = nanoid(8);
     const ext = path.extname(file.originalname);
-    cb(null, `${id}${ext}`);
+    cb(null, `videos/${id}${ext}`);
   }
 });
 
